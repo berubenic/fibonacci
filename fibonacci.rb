2649c03fb68acc9ab2ceb6e446e5cc33698d379b
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-10-02 17:58:07 -0400

    method works, need to fix if n == 0 and n == 1

---
+++ fibonacci.rb
@@ -0,0 +1,20 @@
+def fib(n, arr = [])
+  arr.push(0, 1)
+  if n >= 2
+    2.upto(n) do |num|
+      sum = arr[num - 2] + arr[num - 1]
+      arr.push(sum)
+    end
+  end
+  arr.slice(0..n - 1)
+end
+
+p fib(0)
+p fib(1)
+p fib(2)
+p fib(3)
+p fib(4)
+p fib(5)
+p fib(6)
+p fib(7)
+p fib(13)
