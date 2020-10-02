f2cffb038f4a6677481f3d2c8848014a990fe857
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-10-02 19:20:58 -0400

    added fib_rec, followed along example

--- fibonacci.rb
+++ fibonacci.rb
@@ -21,3 +21,20 @@ p fib(5)
 p fib(6)
 p fib(7)
 p fib(13)
+
+def fib_rec(n)
+  return [] if n.zero?
+  return [0] if n == 1
+  return [0, 1] if n == 2
+
+  next_num = fib(n - 1)
+  next_num.push((next_num[-1]) + (next_num[-2]))
+end
+
+p fib_rec(1)
+p fib_rec(2)
+p fib_rec(3)
+p fib_rec(4)
+p fib_rec(5)
+p fib_rec(6)
+p fib_rec(13)
